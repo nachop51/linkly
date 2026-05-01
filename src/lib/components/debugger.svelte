f1110024edd2aa812270d562218d932e02c468fe
nachop51 <nachoperalta0@gmail.com>
2026-05-01 16:40:37 +0400

    destroy project :)

--- src/lib/components/debugger.svelte
+++
@@ -1,5 +0,0 @@
-<!-- <script lang="ts">
-	import { signOut } from '@auth/sveltekit/client'
-</script>
-
-<button class="btn btn-primary" on:click={() => signOut()}> logout </button> -->
--- src/routes/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import type { PageServerLoad } from './$types'
-
-export const load = (async ({ parent }) => {
-	const { user } = await parent()
-
-	return { user }
-}) satisfies PageServerLoad
